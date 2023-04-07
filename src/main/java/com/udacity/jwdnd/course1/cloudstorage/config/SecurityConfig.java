f32c8f1a3d731f364884bc4e16870100b75e44b5
gyda13 <gyda2100@gmail.com>
2023-04-07 04:34:58 +0300

    user access using spring security

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+
+import com.udacity.jwdnd.course1.cloudstorage.service.AuthenticationService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationService authenticationService;
+
+    public SecurityConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(this.authenticationService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/signup", "/css/**", "/js/**" ,  "/h2-console/**").permitAll()
+                .anyRequest().authenticated();
+
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.formLogin()
+                .loginPage("/login")
+                .permitAll();
+
+        http.formLogin()
+                .defaultSuccessUrl("/home", true);
+
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/login");
+
+    }
+
+}
