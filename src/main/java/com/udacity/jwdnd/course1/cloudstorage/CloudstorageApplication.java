d4f865d8c8216e510d13c157614412f4bc141d88
gyda13 <gyda2100@gmail.com>
2023-04-07 04:28:52 +0300

    Initial commit

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudstorageApplication.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudstorageApplication.java
@@ -1,9 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class CloudstorageApplication {
 
 	public static void main(String[] args) {
