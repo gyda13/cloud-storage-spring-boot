718464fe5c1889e0fe1dc653c8c9f51c82708b20
gyda13 <gyda2100@gmail.com>
2023-04-12 06:59:54 +0300

    update

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -1,13 +1,14 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
-import com.udacity.jwdnd.course1.cloudstorage.service.UserService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller()
 @RequestMapping("/signup")
@@ -24,27 +25,29 @@ public class SignupController {
         return "signup";
     }
 
-    @PostMapping()
-    public String signupUser(@ModelAttribute User user, Model model) {
+
+    @PostMapping
+    public String signupUser(@ModelAttribute User user, RedirectAttributes redirectAttributes){
         String signupError = null;
+        if(!userService.isUsernameAvailable(user.getUsername())){
+            signupError = "The user already exist";
 
-        if (!userService.isUsernameAvailable(user.getUsername())) {
-            signupError = "The username already exists.";
         }
+        if(signupError == null){
+            int rowAdded = userService.createUser(user);
+            if(rowAdded < 0){
+                signupError = "There was a error during signup process. Please try again!";
+            }
 
-        if (signupError == null) {
-            int rowsAdded = userService.createUser(user);
-            if (rowsAdded < 0) {
-                signupError = "There was an error signing you up. Please try again.";
+            if(signupError == null){
+
+                redirectAttributes.addFlashAttribute("signupSuccess", true);
+                redirectAttributes.addFlashAttribute("successMessage", "Account have been created. Please login to continue");
+                return "redirect:/login";
             }
         }
+        redirectAttributes.addFlashAttribute("signupError",signupError);
+        return "redirect:/signup";
 
-        if (signupError == null) {
-            model.addAttribute("signupSuccess", true);
-        } else {
-            model.addAttribute("signupError", signupError);
-        }
-
-        return "signup";
     }
 }
