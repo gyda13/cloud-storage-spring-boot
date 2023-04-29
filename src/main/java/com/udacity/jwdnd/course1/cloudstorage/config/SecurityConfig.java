d4c6ea468af71cdef3a0ae3a3dde5f16f97e1308
gyda13 <gyda2100@gmail.com>
2023-04-29 09:00:52 +0300

    update

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/signup", "/css/**", "/js/**" ,  "/h2-console/**").permitAll()
+                .antMatchers("/signup", "/css/**", "/static/css/js/**",  "/h2-console/**").permitAll()
                 .anyRequest().authenticated();
 
 
